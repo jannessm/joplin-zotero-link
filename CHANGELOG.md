b786b887bc66756238ab1c0a33f230969448b58b
Todd <sceadgugenga@gmail.com>
2025-01-12 16:18:03 -0800

    Added ability to use citation styles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
-# 2.1.0
+# 2.1.3
 
-- Added support for custom format link.
-- Removed `author` filter on item to include other type of creator.
+- Added a variable for <authorslastnames> which is a comma delineated string of author's last name
+- Added automatic creation of citations formatted in accordance to citation style from Zotero Citation Repository--Zotero >= 7 only
+- Added a plugin setting that accepts the name of a format form the Zotero Citation Repository (e.g. apa, chicago-author-date) 
+- Added a plugin setting to allow the customization of the autocomplete hint--Zotero >= 7 only
+- Added a plugin setting to allow the customization of the autocomplete hint sort order--Zotero >= 7 only
+- Added a plugin setting to allow the customization of the addition details to show with the auto complete hint--Zotero >= 7 only
--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,11 @@ import { SettingItemType } from 'api/types';
 
 export enum SETTING {
 	Port = 'zotero-port',
-    CustomFormat = 'zotero-custom-format'
+    CustomFormat = 'zotero-custom-format',
+    CustomHintFormat = 'zotero-custom-hint-format',
+    CitationStyle = 'zotero-citation-style',
+    CustomHintSort = 'zotero-custom-hint-sort',
+    CustomHintDetails = 'zotero-custom-hint-formatdetails'
 };
 
 export const registerSettings = async () => {
@@ -22,13 +26,47 @@ export const registerSettings = async () => {
             type: SettingItemType.String,
             label: 'Port of Zotero 7 API',
         },
+        [SETTING.CitationStyle]: {
+            section: sectionName,
+            value: '',
+            public: true,
+            type: SettingItemType.String,
+            label: 'Citation Style',
+            description: 'Allows you to format citations using Zotero Style Repository. Examples: apa, chicago-author-date, modern-language-association, ieee, american-medical-association'
+        },
         [SETTING.CustomFormat]: {
             section: sectionName,
             value: '[<title>](<zoterolink>)',
             public: true,
             type: SettingItemType.String,
-            label: 'Custom format',
-            description: 'Customize the text links using variables such as <author>, <title> and <date>.'
+            label: 'Custom Format',
+            description: 'Customize the text links using variables such as <authors>, <title> and <date>.'
         },
+        [SETTING.CustomHintFormat]: {
+            section: sectionName,
+            value: '<authors>',
+            public: true,
+            type: SettingItemType.String,
+            label: 'Custom Hint Format',
+            description: 'Customize the hints shown when you use @Z using variables such as <authors>, <title> and <date>.'
+        },
+        [SETTING.CustomHintDetails]: {
+            section: sectionName,
+            value: '',
+            public: true,
+            type: SettingItemType.String,
+            label: 'Custom Hint Details',
+            description: 'Customize additional details you will see in your hints'
+        },
+        [SETTING.CustomHintSort]: {
+            section: sectionName,
+            value: '',
+            public: true,
+            type: SettingItemType.String,
+            label: 'Custom Hint Sort',
+            description: 'Customize the sort order of the autocomplete hints shown.'
+        },
+        
+        
     });
 };
